5cea6a497669432d4f016e6d1109bbe070971d9e
Matthew Radulovich <daemon6109@gmail.com>
2025-06-20 19:19:18 -0500

    Delete places/afk/out/afk/src directory

--- places/afk/out/afk/src/client/runtime.client.lua
+++
@@ -1,7 +0,0 @@
--- Compiled with roblox-ts v2.3.0
-local TS = require(game:GetService("ReplicatedStorage"):WaitForChild("rbxts_include"):WaitForChild("RuntimeLib"))
-local Flamework = TS.import(script, game:GetService("ReplicatedStorage"), "rbxts_include", "node_modules", "@flamework", "core", "out").Flamework
-Flamework._addPaths({ { "ReplicatedStorage", "rbxts_include", "node_modules", "Project", "afk", "src", "client", "components" } })
-Flamework._addPaths({ { "ReplicatedStorage", "rbxts_include", "node_modules", "Project", "afk", "src", "client", "controllers" } })
-Flamework._addPaths({ { "ReplicatedStorage", "rbxts_include", "node_modules", "Project", "afk", "src", "shared", "components" } })
-Flamework.ignite()
--- places/afk/out/afk/src/jest.config.lua
+++
@@ -1,5 +0,0 @@
--- Compiled with roblox-ts v2.3.0
-return {
-	testMatch = { "**/tests/*.(spec|test)" },
-	testPathIgnorePatterns = { "rbxts_include", "node_modules" },
-}
