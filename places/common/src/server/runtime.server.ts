2aa815aea31edaa444e1c85e97ea73d5e056e718
Matthew Radulovich <daemon6109@gmail.com>
2025-06-21 19:38:03 +0000

    fixed runtime.server.ts

--- places/common/src/server/runtime.server.ts
+++ places/common/src/server/runtime.server.ts
@@ -1,7 +1,7 @@
 import { Flamework } from "@flamework/core";
 
-Flamework.addPaths("./places/common/src/client/components");
-Flamework.addPaths("./places/common/src/client/controllers");
+Flamework.addPaths("./places/common/src/server/components");
+Flamework.addPaths("./places/common/src/server/services");
 Flamework.addPaths("./places/common/src/shared/components");
 
 Flamework.ignite();
