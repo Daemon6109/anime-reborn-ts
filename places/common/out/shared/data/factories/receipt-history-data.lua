4d896d35524d8a7c378f76596ba377b0941a323e
Matthew Radulovich <daemon6109@gmail.com>
2025-06-20 19:19:46 -0500

    Delete places/common/out directory

--- places/common/out/shared/data/factories/receipt-history-data.lua
+++
@@ -1,8 +0,0 @@
--- Compiled with roblox-ts v2.3.0
--- Receipt history data factory
-local function createReceiptHistoryData()
-	return {}
-end
-return {
-	createReceiptHistoryData = createReceiptHistoryData,
-}
